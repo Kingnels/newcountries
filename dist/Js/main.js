762a5c385d9678a931a3be05f95ff9a8f1c8138e
Kingnels <ifeanyiibe2022@gmail.com>
2022-09-24 16:10:05 +0100

    commit

--- dist/Js/main.js
+++ dist/Js/main.js
@@ -3,22 +3,26 @@ let cardWrapper = document.getElementById('flags');
 let searchInput = document.getElementById('search');
 
 function populate(){
-    fetch("countries.json", {mode:"cors"})
+    // fetch("countries.json", {mode:"cors"})
+    fetch("https://restcountries.com/v3.1/all", {mode:"cors"})
+
     .then((res) => res.json())
     .then((data) => {
-        //console.log(data);
+        console.log(data);
        
-        let cards = "<div></div>";
+        let cards = "<div style='position:absolute'></div>";
         data.map((x) => {
+            // console.log(x)
+    
             cards += `
-            <a href='#'>
+            <a href='#' style = 'text-decoration: none'>
             <div class="card">
             <div class="image">
-                <img src="${x.flag}" alt="">
+                <img src="${x.flags.png}" alt="">
             </div>
 
             <div class="detail">
-                <h2>${x.name}</h2>
+                <h2>${x.name.common}</h2>
                 <p> <span>Population: </span>${x.population}</p>
                 <p> <span>Region: </span>${x.region}</p>
                 <p> <span>Capital: </span>${x.capital}</p>
@@ -41,26 +45,26 @@ populate();
 searchInput.addEventListener("keyup",()=>{
 //    console.log()
 
-fetch("countries.json", {mode:"cors"})
+fetch("https://restcountries.com/v3.1/all", {mode:"cors"})
 .then((res) => res.json())
 .then((data) => {
     //console.log(data);
    
-    let cards = "<div></div>";
+    let cards = "<div style='position:absolute'></div>";
     // let count = 0;
     data.map((x) => {
-       if(x.name.toLowerCase().includes(searchInput.value.toLowerCase())){
+       if(x.name.common.toLowerCase().includes(searchInput.value.toLowerCase())){
            //count++;
            
             cards += `
-            <a href='#'>
+            <a href='#' style = 'text-decoration: none'>
             <div class="card">
             <div class="image">
-                <img src="${x.flag}" alt="">
+                <img src="${x.flags.png}" alt="">
             </div>
 
             <div class="detail">
-                    <h2>${x.name}</h2>
+                    <h2>${x.name.common}</h2>
                     <p> <span>Population: </span>${x.population}</p>
                     <p> <span>Region: </span>${x.region}</p>
                     <p> <span>Capital: </span>${x.capital}</p>
